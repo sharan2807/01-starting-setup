72df982afc0a4f36c98ddeed0f914299380a59b5
sharan2807 <sharancold98@gmail.com>
2023-11-21 16:28:02 +0530

    Adding basic css styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,17 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
   return (
-    <div>
-      <h2>Expense Items</h2>
+    <div className='expense-item'>
+        <div>December 7th 1998</div>
+        <div className='expense-item__description'>
+            <h2>Car Insurance</h2>
+        </div>
+        <div className="expense-item__price">$294.67</div>
+      {/* <h2>Expense Items</h2>
       <h5>Food Rs 10</h5>
       <h5>Petrol Rs 100</h5>
-      <h5>Movies Rs 200</h5>
+      <h5>Movies Rs 200</h5> */}
     </div>
   );
 }

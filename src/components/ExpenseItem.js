f31ce78622b2ec46733d40a9ffd8d3f524cb5c25
sharan2807 <sharancold98@gmail.com>
2023-11-21 15:55:31 +0530

    Expenses added underneath the Expense Item Header

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense Items</h2>
+      <h5>Food Rs 10</h5>
+      <h5>Petrol Rs 100</h5>
+      <h5>Movies Rs 200</h5>
+    </div>
+  );
+}
+export default ExpenseItem;

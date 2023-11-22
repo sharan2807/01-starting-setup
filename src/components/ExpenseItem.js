a589ac9731bb0039d65cd6ca82208cc5a6145a17
sharan2807 <sharancold98@gmail.com>
2023-11-22 15:07:49 +0530

    Creating ExpenseDate.js comp. & adding .css style

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,4 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
@@ -6,16 +7,18 @@ function ExpenseItem(props) {
     // const expenseAmount=294.67;
     // const LocationOfExpenditure="Goa"
 
-    const month= props.date.toLocaleString('en-US',{month: 'long'})
-    const day= props.date.toLocaleString('en-US',{day: '2-digit'})
-    const year=props.date.getFullYear();
+    // const month= props.date.toLocaleString('en-US',{month: 'long'})
+    // const day= props.date.toLocaleString('en-US',{day: '2-digit'})
+    // const year=props.date.getFullYear();
+
     return (
     <div className='expense-item'>
-        <div>
+        {/* <div>
             <div>{month}</div>
             <div>{day}</div>
             <div>{year}</div>
-        </div>
+        </div> */} 
+        <ExpenseDate date={props.date}></ExpenseDate>
         <div className='expense-item__description'>
             <h2>{props.title}</h2>
         </div>

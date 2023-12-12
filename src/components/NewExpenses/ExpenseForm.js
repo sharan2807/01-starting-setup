37d5120f7332ab553634047465fd065f28731461
sharan2807 <sharancold98@gmail.com>
2023-12-12 22:07:22 +0530

    Clearing old val from i/p form using 2 way binding

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -5,12 +5,11 @@ const ExpenseForm = () => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
-    //  const [userInput,setUserInput] = useState({
-    //     enteredTitle: '',
-    //     enteredAmount: '',
-    //     enteredDate: ''
-    //  })
-
+  //  const [userInput,setUserInput] = useState({
+  //     enteredTitle: '',
+  //     enteredAmount: '',
+  //     enteredDate: ''
+  //  })
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
@@ -38,21 +37,28 @@ const ExpenseForm = () => {
     //     enteredDate:event.target.value
     // });
   };
-  const test=(event)=>{
+  const test = (event) => {
     event.preventDefault();
-    const expenseData={
-        "Title": enteredTitle,
-        "Amount": enteredAmount,
-        "Date":enteredDate
-    }
-    console.log(expenseData)
-  }
+    const expenseData = {
+      Title: enteredTitle,
+      Amount: enteredAmount,
+      Date: enteredDate,
+    };
+    console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
   return (
     <form onSubmit={test}>
       <div className="new-expense__controls">
         <div className="new-expense__controls">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__controls">
           <label>Amount</label>
@@ -60,6 +66,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -69,6 +76,7 @@ const ExpenseForm = () => {
             type="date"
             min="2020-01-01"
             max="2023-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>

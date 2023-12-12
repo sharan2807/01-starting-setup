3e975da7b4bd05563f48c8b259b088a35c089b78
sharan2807 <sharancold98@gmail.com>
2023-12-12 19:21:42 +0530

    2nd way of using 1 state instead of 3[func.Syntax]

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -14,10 +14,13 @@ const ExpenseForm = () => {
 
   const titleChangeHandler = (event) => {
     // setEnteredTitle(event.target.value);
-    setUserInput({
-        ...userInput,
-        enteredTitle:event.target.value
-    });
+    // setUserInput({
+    //     ...userInput,
+    //     enteredTitle:event.target.value
+    // });
+    setUserInput((prevState)=>{
+        return{...prevState,enteredTitle:event.target.value}
+    })
   };
 
   const amountChangeHandler = (event) => {

2cface97eea38fa16cc4fff95b470b736bf3971c
sharan2807 <sharancold98@gmail.com>
2023-12-17 17:40:19 +0530

    Adding <ExpenseList> so Expenses.js code is lesser

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,45 +1,51 @@
-import React from 'react';
+import React from "react";
 
-import ExpenseDate from './ExpenseDate';
-import ExpenseDetails from './ExpenseDetails';
-import './ExpenseItem.css'
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
+import "./ExpenseItem.css";
 
-import Card from '../UI/Card';
+import Card from "../UI/Card";
 
-const ExpenseItem = (props)=> {
-    // const expenseDate="December 7th 1998";
-    // const expenseTitle="Car Insurance";
-    // const expenseAmount=294.67;
-    // const LocationOfExpenditure="Goa"
+const ExpenseItem = (props) => {
+  // const expenseDate="December 7th 1998";
+  // const expenseTitle="Car Insurance";
+  // const expenseAmount=294.67;
+  // const LocationOfExpenditure="Goa"
 
-    // const month= props.date.toLocaleString('en-US',{month: 'long'})
-    // const day= props.date.toLocaleString('en-US',{day: '2-digit'})
-    // const year=props.date.getFullYear();
+  // const month= props.date.toLocaleString('en-US',{month: 'long'})
+  // const day= props.date.toLocaleString('en-US',{day: '2-digit'})
+  // const year=props.date.getFullYear();
 
-    // const [title,setTitle] = useState(props.title);
+  // const [title,setTitle] = useState(props.title);
 
-    // const clickHandler=()=>{
-    //     setTitle('Updated');
-    //     console.log(title);
-    // };
+  // const clickHandler=()=>{
+  //     setTitle('Updated');
+  //     console.log(title);
+  // };
 
-    return (
-    <Card className='expense-item'>
+  return (
+    <li>
+      <Card className="expense-item">
         {/* <div>
             <div>{month}</div>
             <div>{day}</div>
             <div>{year}</div>
-        </div> */} 
+        </div> */}
         <ExpenseDate date={props.date}></ExpenseDate>
         {/* <div className='expense-item__description'>
             <h2>{props.title}</h2>
         </div> */}
-        <ExpenseDetails title={props.title} amount={props.amount}  location={props.location}/>
+        <ExpenseDetails
+          title={props.title}
+          amount={props.amount}
+          location={props.location}
+        />
         {/* <div className='expense-item__location'>{props.location}</div>
 
-        <div className="expense-item__price">${props.amount}</div> */} 
+        <div className="expense-item__price">${props.amount}</div> */}
         {/* <button onClick={clickHandler}>Update </button> */}
-    </Card>
+      </Card>
+    </li>
   );
-}
+};
 export default ExpenseItem;
